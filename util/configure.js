28e23aa0ca22796331dbd92c05687ea583e6d42a
mike benjamin <mike@coolfront.com>
2018-09-13 08:29:42 -0400

    Fix Bug #474
    librkafka++.so.1 not searching for librdkafka.so.1 correctly

--- util/configure.js
+++ util/configure.js
@@ -9,6 +9,7 @@ var baseDir = path.resolve(__dirname, '../');
 var releaseDir = path.join(baseDir, 'build', 'deps');
 
 var isWin = /^win/.test(process.platform);
+var isLinux = /linux/.test(process.platform);
 
 // Skip running this if we are running on a windows system
 if (isWin) {
@@ -19,8 +20,11 @@ if (isWin) {
 var childProcess = require('child_process');
 
 try {
-  childProcess.execSync('./configure --prefix=' + releaseDir + ' --libdir=' + releaseDir, {
-    cwd: baseDir,
+  //for linux we want to ensure we'll find the desired libkafka.so.1 file. To do this we'are going to tell librdkafka++.so.1 to look in its current path at runtime as an additional location to search for libraries.
+  var ENVVARS = isLinux ? "LDFLAGS='-Wl,-rpath,$ORIGIN' " : ""
+  childProcess.execSync(ENVVARS +  "./configure --prefix=" + releaseDir + ' --libdir=' + releaseDir, {
+
+  cwd: baseDir,
     stdio: [0,1,2]
   });
   process.exit(0);
